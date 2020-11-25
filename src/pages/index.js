a38991113ef829246c8c808044dbe3aa6e26fcb5
Felipe Oliveira <felpd3v@gmail.com>
2020-11-25 16:57:11 -0300

    feat(): Adds a hello_world index page

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+const Index = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ad
+        neque. Quos aut quia veniam recusandae cupiditate. Consequuntur
+        pariatur, quod molestias delectus, perferendis hic at non quibusdam
+        error vel voluptatum.
+      </p>
+    </>
+  )
+}
+
+export default Index
